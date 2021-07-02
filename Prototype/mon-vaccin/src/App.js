0959456bc163524ac2ada5ae975ba3525f3aef02
dahrif <dahri.fatima.dev@gmail.com>
2021-07-02 16:09:37 +0100

    prototype

---
+++ Prototype/mon-vaccin/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import sidebar from "./components/sidebar/sidebar";
+
+function App() {
+  return 
+    <div className="App"><sidebar /></div>;
+}
+
+export default App;
---
+++ Prototype/mon-vaccin/src/components/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./sidebar.css";
+
+
+function sidebar(){
+    return(
+        <div id="sidebar">
+lhjlkjlk
+        </div>
+    )
+}
+export default sidebar;
