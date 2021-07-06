3623a68095c0e6a03fc93a0bc094e0e749866aa7
dahrif <dahri.fatima.dev@gmail.com>
2021-07-06 11:00:02 +0100

    maquette / conception / prototype

--- Prototype/mon-vaccin/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import sidebar from "./components/sidebar/sidebar";
-
-function App() {
-  return 
-    <div className="App"><sidebar /></div>;
-}
-
-export default App;
---
+++ Prototype/sidebar.js
@@ -0,0 +1,5 @@
+class Profile extends React.Component{
+    render(){
+        return <h1></h1>;
+    }
+}
