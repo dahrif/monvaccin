5fcde01b3af528c081a828ed69f7a30dc8675807
dahrif <dahri.fatima.dev@gmail.com>
2021-07-19 16:57:53 +0100

    save

--- Realisation/test/api/api-vaccination/deletevaccination.php
+++ Realisation/test/api/api-vaccination/deletevaccination.php
@@ -1,6 +1,8 @@
 <?php 
 require_once (__DIR__.'/../../manager/vaccinationmanager.php');
 
+$vaccination = new Vaccination();
+$vaccination->getdate_vaccination($_POST["date_vaccination"]);
 $deletevaccinationManager = null;
 $deletevaccinationManager =  new vaccinationManager(); 
 $deletevaccinationQuery = $deletevaccinationManager->delete($_POST["date_vaccination"]);
