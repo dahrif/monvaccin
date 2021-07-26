0951567dea6b8d084c2595701f74e00727117f1a
dahrif <dahri.fatima.dev@gmail.com>
2021-07-26 17:03:59 +0100

    save changes

---
+++ code-source/Realisation/codee/js/app2.js
@@ -0,0 +1,1 @@
+ReactDOM.render(<Acceuil />, document.getElementById('app'));
---
+++ code-source/Realisation/codee/js/enfant1.component.js
@@ -0,0 +1,39 @@
+class Enfant1 extends React.Component {
+    constructor(props) {
+      super(props)
+    }
+  
+    render() {
+  
+      return (
+
+      <div className="container">
+
+<div className="card col-md-12 mx-auto">
+
+
+<div className="row">
+  <div className="col-md-4 my-3 mx-auto">
+    <img src="https://image.flaticon.com/icons/png/512/405/405129.png" width="120px" alt="user-avatar" className="img-circle img-fluid" />
+  </div>
+  <div className=" col-7 mt-2 ml-3">
+    <h2 className="lead mt-2"><b>Nom: SOUJOUD</b></h2>
+    <h2 className="lead mt-2"><b>Date de naissance : 13/01/2020</b></h2>
+    <h2 className="lead mt-2"><b>Poids à la naissance : 2.8 kg</b></h2>
+  </div>
+  <div className="col-12 text-left">
+
+
+
+  </div>
+
+</div>
+
+
+
+</div>
+         
+            </div>
+      )
+    }
+  }
